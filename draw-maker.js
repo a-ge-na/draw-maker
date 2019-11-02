fed6dea398ade9dbe3c4b50ea3934eac94558ea7
a-ge-na <github@ho.orz.hm>
2019-11-02 13:48:50 +0900

    評価関数の枠作成

--- draw-maker.js
+++ draw-maker.js
@@ -8,6 +8,11 @@ function Pair(player1, team1, player2, team2, seed) {
     this.player2 = player2;
     this.team2 = team2;
     this.seed = seed;
+
+    this.getFriendship = function(obj){
+        console.log(obj);
+    }
+    
 }
 
 /**
@@ -17,6 +22,7 @@ window.onload = function() {
     var button = document.getElementById('button-go');
     button.addEventListener('click', function() {
         var data = text2array();
+        data = makeDraw(data);
         array2table(data);
     });
 }
@@ -26,7 +32,9 @@ window.onload = function() {
  */
 function text2array() {
     var textarea = document.getElementById('input-text');
-    var lines = textarea.value.split(/\r\n|\n/).filter(line => line.trim().length > 0);
+    var lines = textarea.value.split(/\r\n|\n/);
+    lines.forEach(element => {element = element.trim()});
+    lines = lines.filter(line => line.trim().length > 0);
     var mode = document.getElementById('mode');
     var objs = Array();
     switch (mode.value) {
@@ -37,7 +45,6 @@ function text2array() {
             var sepaletor = ",";
             break;
     }
-    //    console.log(lines);
     lines.forEach(element => {
         var token = element.split(sepaletor);
         var i = 0;
@@ -48,8 +55,19 @@ function text2array() {
 }
 
 /**
+ * 
+ * @param {*} data 
+ */
+function makeDraw(data){
+    data.forEach(element => {
+        console.log(element.getFriendship(data));
+    });
+    return data;
+}
+
+/**
  * 配列をテーブルに出力
  */
 function array2table(data) {
-    console.log(data);
+//    console.log(data);
 }
