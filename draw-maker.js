428881f3000e2e4c2704ba3880b6b50b4e0b6405
a-ge-na <github@ho.orz.hm>
2019-11-06 21:13:53 +0900

    評価関数の実装とブロック分けの実装

--- draw-maker.js
+++ draw-maker.js
@@ -1,4 +1,3 @@
-
 /**
  * ダブルスペアを表すクラス
  * 
@@ -8,8 +7,8 @@
  * @param {String} team2 
  * @param {Number} seed 
  */
-class Pair extends Object{
-    constructor(player1, team1, player2, team2, seed){
+class Pair extends Object {
+    constructor(player1, team1, player2, team2, seed) {
         super();
         this.player1 = player1;
         this.team1 = team1;
@@ -18,28 +17,29 @@ class Pair extends Object{
         this.seed = seed;
     }
 
-    getFriendship(obj){
-        if(obj === this){
+    getFriendship(obj) {
+        if (obj === this) {
             return 0;
         }
-        if(obj instanceof Array){
+        if (obj instanceof Array) {
             var sum = 0;
             obj.forEach(element => {
                 sum += this.getFriendship(element);
             });
             return sum;
-        }else if(obj instanceof Pair){
+        } else if (obj instanceof Pair) {
             var score = 0;
             score += (this.team1 == obj.team1) ? 1 : 0;
             score += (this.team1 == obj.team2) ? 1 : 0;
             score += (this.team2 == obj.team1) ? 1 : 0;
             score += (this.team2 == obj.team2) ? 1 : 0;
+            score *= (this.team1 == this.team2) ? 2 : 1;
             return score;
-        }else{
+        } else {
             console.log('error');
         }
     }
-    
+
 }
 
 /**
@@ -47,10 +47,23 @@ class Pair extends Object{
  * 
  * @param {Numver} capacity ブロック内のユニット数
  */
-class Block extends Array{
-    constructor(capacity){
-        super(capacity);
-    }        
+class Block extends Array {
+    constructor() {
+        super();
+    }
+
+    /**
+     * ペアの拒否レベル
+     * 
+     * @param {Pair} Obj 
+     */
+    getRefusalScore(Obj) {
+        var score = 0;
+        score += 10000 * this.length;
+        score += 100 * Obj.getFriendship(this);
+        score += 1 * 0;
+        return score;
+    }
 }
 /**
  * 
@@ -72,7 +85,7 @@ window.onload = function() {
 function text2array() {
     var textarea = document.getElementById('input-text');
     var lines = textarea.value.split(/\r\n|\n/);
-    lines.forEach(element => {element = element.trim()});
+    lines.forEach(element => { element = element.trim() });
     lines = lines.filter(line => line.trim().length > 0);
     var mode = document.getElementById('mode');
     var objs = Array();
@@ -97,28 +110,22 @@ function text2array() {
  * 
  * @param {*} data 
  */
-function makeDraw(data){
-//    switch(data.length){
-//    case 6:
-        var blocks = Array(new Block(3), new Block(3));
-        //        break;
-//    }
-    // TODO: テーブルを作る
-    // テーブルの配列を返す
-    // 
-    data.sort(function(a, b){
-/*
-        seed順を最優先
-        同チームの多い方を優先
-        その他・・・
-*/
-        // SEED
-        return b.getFriendship(data) - a.getFriendship(data);
+function makeDraw(data) {
+    var blockcount = Math.ceil(data.length / 4);
+    var blocks = [...Array(blockcount)].map(x => new Block());  // blockcount個のBlockの配列
+    var data2 = Array.from(data);
+    data2.sort(function(a, b) {
+        return b.getFriendship(data2) - a.getFriendship(data2);
     });
-    data.forEach(element => {
-        console.log(element,element.getFriendship(data));
-    });
-    return data;
+    while(pair = data2.shift()){
+        var block = blocks.reduce((a, b) => a.getRefusalScore(pair) <= b.getRefusalScore(pair) ? a : b);
+        block.push(pair);
+        data2.sort(function(a, b) {
+            return b.getFriendship(data2) - a.getFriendship(data2);
+        });
+    }
+console.log(blocks);
+return blocks;
 }
 
 /**
